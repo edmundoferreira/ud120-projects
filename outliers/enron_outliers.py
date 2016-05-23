f55b81683df929e945095fe65b35f8d10e01d60b
edmundoferreira <fc.edmundo@gmail.com>
2016-05-23 21:23:38 +0200

    finished outliners mini projects

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,33 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
-data = featureFormat(data_dict, features)
+
 
 
 ### your code below
+for j in range(0,3):
+    max_i = 0
+    max_e = 0
+    for i in data_dict:
+        if max_e < data_dict[i]["salary"] and data_dict[i]["salary"]!='NaN':
+            max_e = data_dict[i]["salary"]
+            max_i = i
+
+    print "outlier(name,salary):",max_i,max_e
+    data_dict.pop(max_i)
+
+
+data = featureFormat(data_dict, features)
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
 
 
+
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,39 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    #chekcing they all the same size
+    import math
+    if not (len(predictions) == len(ages) == len(net_worths)):
+        print "dataset size mismatch"
 
-    
+
+
+    for i in range(0,len(ages)):
+        cleaned_data.append((ages[i], net_worths[i], math.pow(net_worths[i]-predictions[i][0],2)))
+
+    orig_size = len(cleaned_data)
+    reduction_number = orig_size - int(orig_size*0.9)
+
+    for j in range(0,reduction_number):
+        max_i = 0
+        max_e = 0
+        for i in range(0,len(cleaned_data)):
+            if max_e < cleaned_data[i][2]:
+                max_e = cleaned_data[i][2]
+                max_i = i
+        del cleaned_data[max_i]
+
+
+    if not len(cleaned_data)+reduction_number == orig_size:
+        print "size reduction of 90%, missmatch"
+
+
+
     return cleaned_data
-
